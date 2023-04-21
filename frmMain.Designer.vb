8786b2566eb63272e93ef17a367471ad2d15a9dc
zero <3406985348@qq.com>
2023-04-22 00:17:54 +0800

    Upload files

---
+++ frmMain.Designer.vb
@@ -0,0 +1,238 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.mnuOpen = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.mnuSave = New System.Windows.Forms.ToolStripButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtNewName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtOldName = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.txtNewID = New System.Windows.Forms.TextBox()
+        Me.txtOldID = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cdlOpen = New System.Windows.Forms.OpenFileDialog()
+        Me.ToolStrip1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuOpen, Me.ToolStripSeparator1, Me.mnuSave})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(778, 33)
+        Me.ToolStrip1.TabIndex = 0
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'mnuOpen
+        '
+        Me.mnuOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.mnuOpen.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.mnuOpen.Name = "mnuOpen"
+        Me.mnuOpen.Size = New System.Drawing.Size(121, 28)
+        Me.mnuOpen.Text = "Open Image"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 33)
+        '
+        'mnuSave
+        '
+        Me.mnuSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.mnuSave.Enabled = False
+        Me.mnuSave.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.mnuSave.Name = "mnuSave"
+        Me.mnuSave.Size = New System.Drawing.Size(112, 28)
+        Me.mnuSave.Text = "Save Image"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtNewName)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtOldName)
+        Me.GroupBox1.Location = New System.Drawing.Point(10, 38)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox1.Size = New System.Drawing.Size(757, 205)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        '
+        'txtNewName
+        '
+        Me.txtNewName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtNewName.Font = New System.Drawing.Font("Microsoft YaHei UI", 9.0!)
+        Me.txtNewName.Location = New System.Drawing.Point(17, 155)
+        Me.txtNewName.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtNewName.Name = "txtNewName"
+        Me.txtNewName.Size = New System.Drawing.Size(722, 30)
+        Me.txtNewName.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft YaHei UI", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Label2.Location = New System.Drawing.Point(18, 129)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(115, 25)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "New Name:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft YaHei UI", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Label1.Location = New System.Drawing.Point(18, 30)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(147, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Original Name:"
+        '
+        'txtOldName
+        '
+        Me.txtOldName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtOldName.Font = New System.Drawing.Font("Microsoft YaHei UI", 9.0!)
+        Me.txtOldName.Location = New System.Drawing.Point(17, 60)
+        Me.txtOldName.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtOldName.Name = "txtOldName"
+        Me.txtOldName.ReadOnly = True
+        Me.txtOldName.Size = New System.Drawing.Size(722, 30)
+        Me.txtOldName.TabIndex = 0
+        Me.txtOldName.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.txtNewID)
+        Me.GroupBox2.Controls.Add(Me.txtOldID)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Location = New System.Drawing.Point(11, 247)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox2.Size = New System.Drawing.Size(756, 86)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        '
+        'txtNewID
+        '
+        Me.txtNewID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtNewID.Font = New System.Drawing.Font("Microsoft YaHei UI", 9.0!)
+        Me.txtNewID.Location = New System.Drawing.Point(593, 34)
+        Me.txtNewID.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtNewID.Name = "txtNewID"
+        Me.txtNewID.Size = New System.Drawing.Size(145, 30)
+        Me.txtNewID.TabIndex = 4
+        '
+        'txtOldID
+        '
+        Me.txtOldID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtOldID.Font = New System.Drawing.Font("Microsoft YaHei UI", 9.0!)
+        Me.txtOldID.Location = New System.Drawing.Point(135, 34)
+        Me.txtOldID.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtOldID.Name = "txtOldID"
+        Me.txtOldID.ReadOnly = True
+        Me.txtOldID.Size = New System.Drawing.Size(145, 30)
+        Me.txtOldID.TabIndex = 4
+        Me.txtOldID.TabStop = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft YaHei UI", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Label4.Location = New System.Drawing.Point(507, 37)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(82, 25)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "New ID:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft YaHei UI", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Label3.Location = New System.Drawing.Point(17, 37)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(114, 25)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Original ID:"
+        '
+        'cdlOpen
+        '
+        Me.cdlOpen.FileName = "OpenFileDialog1"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(778, 344)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.Text = "NGC/Wii Game ID"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents mnuOpen As ToolStripButton
+    Friend WithEvents mnuSave As ToolStripButton
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtOldName As TextBox
+    Friend WithEvents txtNewName As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents txtNewID As TextBox
+    Friend WithEvents txtOldID As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents cdlOpen As OpenFileDialog
+End Class
---
+++ frmMain.vb
@@ -0,0 +1,135 @@
+﻿Public Class frmMain
+    Private fileName As String
+    Private Sub ToolStripLabel1_Click(sender As Object, e As EventArgs) Handles mnuOpen.Click
+        On Error GoTo Err
+        txtOldName.Text = ""
+        txtOldID.Text = ""
+
+        cdlOpen.Multiselect = False
+        cdlOpen.Filter = "Any file(*.*)|*.*"
+        cdlOpen.ShowDialog()
+
+        If cdlOpen.FileName <> "" Then
+            fileName = cdlOpen.FileName
+            mnuSave.Enabled = True
+            ReadImg()
+        End If
+
+Err:
+        Exit Sub
+    End Sub
+
+    Private Sub ReadImg()
+        Dim gameName(0 To 99) As Byte
+        Dim strGameName As String
+        Dim gameID(0 To 5) As Byte
+        Dim strGameID As String
+        Dim fileHead(0 To 3) As Byte
+        Dim i As Integer
+
+        Try
+            FileSystem.FileOpen(1, fileName, OpenMode.Binary)
+            FileSystem.FileGet(1, fileHead)
+            ' The offset for wbfs file is different with iso or gcm file
+            If fileHead(0) = 87 And fileHead(1) = 66 And fileHead(2) = 70 And fileHead(3) = 83 Then
+                FileSystem.Seek(1, 2097153)
+                FileSystem.FileGet(1, gameID)
+                FileSystem.Seek(1, 2097185)
+                FileSystem.FileGet(1, gameName)
+            Else
+                FileSystem.Seek(1, 1)
+                FileSystem.FileGet(1, gameID)
+                FileSystem.Seek(1, 33)
+                FileSystem.FileGet(1, gameName)
+            End If
+            FileSystem.FileClose(1)
+
+            strGameID = ""
+            For i = 0 To 5
+                strGameID = strGameID + Chr(gameID(i))
+            Next
+            txtOldID.Text = strGameID
+            txtNewID.Text = strGameID
+
+            strGameName = ""
+            For i = 0 To 99
+                If gameName(i) = 0 Then
+                    Exit For
+                End If
+                strGameName = strGameName + Chr(gameName(i))
+            Next
+            txtOldName.Text = strGameName
+            txtNewName.Text = strGameName
+
+        Catch IOExcep As IO.IOException
+            FileSystem.FileClose(1)
+            Exit Sub
+        End Try
+
+    End Sub
+
+    Private Sub mnuSave_Click(sender As Object, e As EventArgs) Handles mnuSave.Click
+        If txtNewID.Text = "" Or txtNewName.Text = "" Or Len(txtNewID.Text) <> 6 Then
+            MsgBox("Invalid value, please check", MsgBoxStyle.Critical, "NGC/Wii Game ID")
+            Exit Sub
+        ElseIf txtNewID.Text = txtOldID.Text And txtNewName.Text = txtOldName.Text Then
+            MsgBox("File not change", MsgBoxStyle.Information, "NGC/Wii Game ID")
+            Exit Sub
+        End If
+
+        SaveImg()
+
+        txtNewID.Text = ""
+        txtNewName.Text = ""
+        txtOldID.Text = ""
+        txtOldName.Text = ""
+        mnuSave.Enabled = False
+    End Sub
+
+    Private Sub SaveImg()
+        Dim gameID(0 To 5) As Byte
+        Dim gameName() As Byte
+        Dim fileHead(0 To 3) As Byte
+        Dim overwriteLength As Integer
+        Dim i As Integer
+
+        Try
+            For i = 0 To 5
+                gameID(i) = Asc(Mid(txtNewID.Text, i + 1, 1))
+            Next
+
+            overwriteLength = IIf(Len(txtOldName.Text) > Len(txtNewName.Text), Len(txtOldName.Text) - 1, Len(txtNewName.Text) - 1)
+            ReDim gameName(0 To overwriteLength)
+            For i = 0 To Len(txtNewName.Text) - 1
+                gameName(i) = Asc(Mid(txtNewName.Text, i + 1, 1))
+            Next
+
+            FileSystem.FileOpen(1, fileName, OpenMode.Binary)
+            FileSystem.FileGet(1, fileHead)
+            ' The offset for wbfs file is different with iso or gcm file
+            If fileHead(0) = 87 And fileHead(1) = 66 And fileHead(2) = 70 And fileHead(3) = 83 Then
+                ' WBFS file need change values in two positions
+                FileSystem.Seek(1, 513)
+                FileSystem.FilePut(1, gameID)
+                FileSystem.Seek(1, 545)
+                FileSystem.FilePut(1, gameName)
+
+                FileSystem.Seek(1, 2097153)
+                FileSystem.FilePut(1, gameID)
+                FileSystem.Seek(1, 2097185)
+                FileSystem.FilePut(1, gameName)
+            Else
+                FileSystem.Seek(1, 1)
+                FileSystem.FilePut(1, gameID)
+                FileSystem.Seek(1, 33)
+                FileSystem.FilePut(1, gameName)
+            End If
+            FileSystem.FileClose(1)
+            MsgBox("Image saved", MsgBoxStyle.Information, "NGC/Wii Game ID")
+        Catch IOExcep As IO.IOException
+            FileSystem.FileClose(1)
+            Exit Sub
+        End Try
+
+    End Sub
+End Class
